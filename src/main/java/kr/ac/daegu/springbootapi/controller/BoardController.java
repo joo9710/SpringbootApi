8df01b0a9388178993cdfc69ff56ca8f18e93ea2
joo9710 <joo9710@naver.com>
2021-09-27 21:08:57 +0900

    logging

--- src/main/java/kr/ac/daegu/springbootapi/controller/BoardController.java
+++ src/main/java/kr/ac/daegu/springbootapi/controller/BoardController.java
@@ -1,14 +1,21 @@
 package kr.ac.daegu.springbootapi.controller;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class BoardController {
 
     @RequestMapping(value = "/")
     public String rootTest() throws Exception {
+        log.trace("logTest~~~~~~~~~~~~~~~~~~~~~~~~~~~~"); // 로그의 5단계 trace(일반적으로 남기는 이벤트 기록) -> debug(디버깅용) -> info(알림) -> warn(경고) -> error(에러)
+        log.debug("logTest~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        log.info("logTest~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        log.warn("logTest~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        log.error("logTest~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         return "hello!";
     }
 }
