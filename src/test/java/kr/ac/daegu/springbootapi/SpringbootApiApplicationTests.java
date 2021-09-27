9327503c805903c0f21d6fca8ac3938b62b3af34
joo9710 <joo9710@naver.com>
2021-09-27 18:47:23 +0900

    initial commit

---
+++ src/test/java/kr/ac/daegu/springbootapi/SpringbootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.daegu.springbootapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
